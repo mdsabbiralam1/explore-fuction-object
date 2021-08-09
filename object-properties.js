c98d3c6188669b618f349ce9a76d5782577a0b0e
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-10 02:53:09 +0600

    add code

---
+++ object-properties.js
@@ -0,0 +1,18 @@
+var computer = {
+    price: 4564,
+    Storage: "500gb",
+    processor: 'intel i5'
+}
+
+// console.log(computer);
+// console.log(computer.processor);
+var computerPrice = computer.price;
+
+// set a object porpertie value;
+computer.price = 7000;
+console.log(computer);
+// console.log(computerPrice);
+
+
+
+su
