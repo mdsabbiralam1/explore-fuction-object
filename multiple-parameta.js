63f5f482ef2acfc3c570c0815c33fa5d4feca84b
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-10 02:26:09 +0600

    added multiple file

---
+++ multiple-parameta.js
@@ -0,0 +1,17 @@
+function addTwoNumbers(number1, number2) {
+    console.log(number1, number2);
+    var total = number1 + number2
+    return total;
+
+}
+var firstNumber = 30;
+var secoundNumber = 20;
+var total = addTwoNumbers(firstNumber, secoundNumber);
+console.log('result value', total);
+
+function multiplyTwoNumbers(num1, num2) {
+    ver result = num1 * num2;
+    return result;
+}
+var total = multiplyTwoNumbers(5, 10);
+console.log('total multiplication,', total);
