d9023d112739ed2128b451a1437e1d7d7c9aae76
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-09 23:54:40 +0600

    added function file

---
+++ function.js
@@ -0,0 +1,18 @@
+// function declaration
+function startFan() {
+    console.log('walk toward the switch')
+    console.log('press the switch');
+}
+// call the function
+startFan();
+console.log('eat breakfast');
+console.log('Drink tea');
+console.log('take a shower');
+startFan();
+var country = "BD";
+var city = "dhaka";
+var location = "gulisthan";
+console.log(country);
+console.log(city);
+console.log(location);
+startFan();
