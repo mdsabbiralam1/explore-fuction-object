60d7b831da7f190d2cc428f96ae3a9ee651b1899
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-09 23:26:22 +0600

    racap file added

--- recap.js
+++ recap.js
@@ -1,3 +1,35 @@
+//1. variable
 var favoritBook = '4 hour work week';
-var booklist = ['positioning', 'hooked', 'start with why']
-var hookedindex = booklist.
+
+// 2. array
+var booklist = ['positioning', 'hooked', 'start with why'];
+var hookedindex = booklist.indexOf('hooked');
+console.log(hookedindex);
+booklist[1] = 'story brand';
+booklist.push('habluder programming', 'nasim vai er book');
+booklist.pop();
+console.log(booklist);
+
+// 3. conditonal
+if (booklist[1] = 'hooked') {
+    console.log('i am hooked');
+}
+else {
+    console.log('i am coder !');
+}
+
+//  4.loop
+//while loop
+var i = 0; //lope variable
+while (i < 5) {
+    console.log(i);
+    console.log('looping looping lopping');
+    i++;
+}
+
+//for loop
+for (j = 0; j < 5; j++) {
+    console.log(i);
+    console.log('hello mama !');
+}
+
