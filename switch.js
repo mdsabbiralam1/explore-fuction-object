05130c5b9b748ae82bf6f5e8e8e68bc7f847470e
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-10 18:25:47 +0600

    add switch file source

---
+++ switch.js
@@ -0,0 +1,23 @@
+var color = 'yellow';
+if (color == 'red') {
+    console.log('color is red');
+}
+
+else if (color == 'yellow') {
+    console.log('color is yellow');
+}
+else if (color == 'white') {
+    console.log('color is white');
+}
+else if (color == 'sky') {
+    console.log('color is sky');
+}
+else if (color == 'green') {
+    console.log('color is green');
+}
+else if (color == 'megenta') {
+    console.log('color is megenta');
+}
+else {
+    console.log('color is black');
+}
