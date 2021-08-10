1b9851fe1a76e7cc6f56220948dc9fd5325b2e5a
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-10 20:16:27 +0600

    main

--- break-condition.js
+++ break-condition.js
@@ -1,7 +1,30 @@
-var i = 0;
-while (i < 15) {
-    console.log(i);
-    if (
-        
-    )
+// var i = 0;
+// while (i < 15) {
+//     console.log(i);
+//     if (i == 5) {
+//         break;
+//     }
+//     i++;
+// }
+// for (j = 0; j < 20; j++) {
+//     console.log(j);
+//     if (j == 8) {
+//         break;
+//     }
+// }
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// for (var i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     console.log(number);
+//     if (number > 100) {
+//         break;
+//     }
+// {
+
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    if (number > 5) {
+        break;
+    }
+    console.log(number);
 }
